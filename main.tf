d5e5e397f73385a28b450dc7ceca51bc3e2eab03
Johannes Rupieper (D056754) <Johannes.Rupieper@sap.com>
2023-01-31 12:01:03 +0100

    remove provider

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.50.4"
-    }
-  }
   backend "gcs" {
     bucket = "orchestrator-tf-state-demo"
     prefix = "terraform/state"
